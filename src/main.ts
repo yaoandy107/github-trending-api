3fc07525088588e49139746422a7c3c63d36167f
yaoandy107 <yaoandy107@gmail.com>
2023-10-06 18:02:12 +0900

    Try to use express in Nest.js

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
     methods: ['GET'],
